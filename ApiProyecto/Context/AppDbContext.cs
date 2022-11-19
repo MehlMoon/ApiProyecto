3027c7b763d9fe7997f9c06e186f65c5889c0ae1
Elsa Aranda M <arandamendozaelsa@gmail.com>
2022-11-19 11:31:27 -0600

    Agregar archivos de proyecto.

---
+++ ApiProyecto/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ApiProyecto.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProyecto.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet <usuarios> usuarios { get; set; }
+
+    }
+}
+ 
---
+++ ApiProyecto/Startup.cs
@@ -0,0 +1,27 @@
+﻿using ApiProyecto.Context;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ApiProyecto
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }  
+
+        public Startup (IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Conexion")));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostApplicationLifetime lifetime)
+        {
+
+        }
+    }
+}
